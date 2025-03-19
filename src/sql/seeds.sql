abc6b6f2dc3c6a40295604ae4504dcc2f50f293d
Sergio Arturo Aguilar Basurto <sergioaguilar@Laptop-de-Sergio.local>
2025-03-18 21:41:08 -0600

    Added seed for populating tables.

---
+++ src/sql/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (name) VALUES ('Marketing'), ('Engineering'), ('Sales'), ('Logistics');
+
+INSERT INTO roles (title, salary, department_id) VALUES ('Publicist', 8000, 11), ('Sr. Software Engineer', 1000000, 12), ('Promoter', 5000, 13), ('Route Planner', 7000, 14);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sergio', 'Aguilar', 7, null), ('Michell', 'Perez' , 6, null);
+
+
